3bece92937fed8b4d88c7b5af09e966066ebf44e
Marco Zamana <marcozamana@microsoft.com>
2022-07-18 12:25:10 +0000

    First commit

---
+++ src/LoadTestK6/loadtest.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 15 },
+    { duration: '20s', target: 15 }
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<250'],
+  },
+};
+
+export default function () {
+  const resFast = http.get(`${__ENV.MY_HOSTNAME}/sampleapi`);
+}
